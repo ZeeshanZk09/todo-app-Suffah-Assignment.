944e29332ff64ada33ff0050432b57d12acbdf27
Muhammad Zeeshan Khan <dr5269139@gmail.com>
2024-08-03 16:23:12 +0500

    todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Input, Button, List } from 'antd';
+import "./App.css"
 
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
+  const [searchValue, setSearchValue] = useState('');
 
   const handleAddTodo = () => {
     if (newTodo.trim()) {
@@ -11,14 +13,19 @@ const App = () => {
       setNewTodo('');
     }
   };
-
+  const handleSearchChange = (e) => {
+        setSearchValue(e.target.value);
+      };
+    const handleInputChange = (e) => {
+      setNewTodo(e.target.value);
+        };
   return (
     <div style={{ padding: '20px' }}>
       <h1>Todo App</h1>
       <Input
         placeholder="Enter a todo"
         value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
+        onChange={handleInputChange}
         onPressEnter={handleAddTodo}
         style={{ width: '300px', marginRight: '10px' }}
       />
@@ -26,9 +33,24 @@ const App = () => {
         Add Todo
       </Button>
       <List
-        style={{ marginTop: '20px', width: '300px' }}
+        style={{ marginTop: '20px', width: '300px',fontWeight: "bold", }}
         bordered
-        dataSource={todos}
+        onChange={handleSearchChange}
+        dataSource={todos
+          .filter((todo) => todo.includes(searchValue))
+          .map((todo, index) => (
+            <li key={index}>
+              <span>
+                <img
+                  src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                  alt="avatar"
+                  width="20"
+                  height="20"
+                />
+                {todo}
+              </span>
+            </li>
+          ))}
         renderItem={(item) => (
           <List.Item>
             {item}
